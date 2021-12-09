7feb334937001890b993be9e9424576a0c9323fa
Reuben Lindroos <rjl67@bath.ac.uk>
2021-12-09 13:40:07 +0000

    removed custom cli for test/main.py

--- Python/preprocessing/test/itests.py
+++ Python/preprocessing/test/itests.py
@@ -7,28 +7,32 @@ import json
 
 class TestPreprocessing(unittest.TestCase):
     def setUp(self) -> None:
-        self.currdir = os.path.dirname(__file__)
-        with open(os.path.join(self.currdir,"paths.json"),"r") as ifile:
-            dct = json.load(ifile)
-        self.config_filepath = os.path.join(dct["test_data_root_dir"],
+        self.curr_dir = os.path.dirname(__file__)
+        self.test_data_root_dir = os.environ["OP_TEST_ROOT_DIR"].replace('"','')
+        self.config_filepath = os.path.join(self.test_data_root_dir,
                                             "python-config.yaml")
     def test_main(self):
-        app = PreprocAPP({"config_file":self.config_filepath,
-                          "headless":True})
-        app.run()
+        try:
+            app = PreprocAPP({"config_file":self.config_filepath,
+                              "headless":True})
+            app.run()
+        except Exception as error:
+            self.fail("Failed with {}".format(error))
+
         self.assertTrue(True, "Integration test passed with no exceptions.")
 
 class TestCircleSelector(unittest.TestCase):
     def setUp(self) -> None:
-        self.currdir = os.path.dirname(__file__)
-        with open(os.path.join(self.currdir,"paths.json"),"r") as ifile:
+        self.curr_dir = os.path.dirname(__file__)
+        with open(os.path.join(self.curr_dir, "paths.json"), "r") as ifile:
             dct = json.load(ifile)
+        self.test_data_root_dir = os.environ["OP_TEST_ROOT_DIR"].replace('"','')
 
-        self.ovslam_csv = os.path.join(dct["test_data_root_dir"],
+        self.ovslam_csv = os.path.join(self.test_data_root_dir,
                                        dct["relative_path_frame_trajectory_csv"])
-        self.best_intervals_json = os.path.join(dct["test_data_root_dir"],
+        self.best_intervals_json = os.path.join(self.test_data_root_dir,
                                                 dct["relative_path_best_intervals_test"])
-        self.data_json = os.path.join(self.currdir,"data.json")
+        self.data_json = os.path.join(self.curr_dir, "data.json")
 
     def test_loader(self):
         if not os.path.exists(self.ovslam_csv):
@@ -48,8 +52,10 @@ class TestCircleSelector(unittest.TestCase):
         # save data for a later test
         data.toJSON(self.data_json)
         self.assertIsNotNone(circleselector.metrics.calc(points,
-                                                         errors=["endpoint_error", "flatness_error",
-                                                                 "perimeter_error","pairwise_distribution"]))
+                                                         errors=["endpoint_error",
+                                                                 "flatness_error",
+                                                                 "perimeter_error",
+                                                                 "pairwise_distribution"]))
 
 if __name__ == '__main__':
     unittest.main()
