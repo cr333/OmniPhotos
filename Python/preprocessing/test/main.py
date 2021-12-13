06a54b12dc78b9aae6ec5a83df8a49b490faa1d3
Reuben Lindroos <rjl67@bath.ac.uk>
2021-12-13 13:45:52 +0000

    testing initialisation from mainfile

--- Python/preprocessing/test/main.py
+++ Python/preprocessing/test/main.py
@@ -1,6 +1,7 @@
 import unittest
 import sys
 import os
+from preproc_app import PreprocAPP
 
 # append the preprocessing directory to the path. this is necessary as unittests "needs" its own mainfile and if this
 # isn't in the root directory the imports don't work.
@@ -13,5 +14,7 @@ from utests import *
 
 if __name__ == '__main__':
     #unittest.main()
-    print(os.environ["OP_TEST_ROOT_DIR"])
-    print(list(os.environ["OP_TEST_ROOT_DIR"]))
+    # print(os.environ["OP_TEST_ROOT_DIR"])
+    # print(list(os.environ["OP_TEST_ROOT_DIR"]))
+    app = PreprocAPP({"config_file":os.environ["OP_TEST_ROOT_DIR"],
+                      "headless":True})
