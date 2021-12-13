71b21be6f8683713e087a188056ac4bbe5d0230f
Reuben Lindroos <rjl67@bath.ac.uk>
2021-12-13 13:52:53 +0000

    fixed issue with import path

--- Python/preprocessing/test/main.py
+++ Python/preprocessing/test/main.py
@@ -1,13 +1,15 @@
 import unittest
 import sys
 import os
+
+path = os.path.split(os.path.dirname(__file__))[0]
+sys.path.append(path)
+
 from preproc_app import PreprocAPP
 
 # append the preprocessing directory to the path. this is necessary as unittests "needs" its own mainfile and if this
 # isn't in the root directory the imports don't work.
 
-path = os.path.split(os.path.dirname(__file__))[0]
-sys.path.append(path)
 
 from itests import *
 from utests import *
